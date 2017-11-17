c39f5405335f5ae4970dcb114ac956b80e5cb732
ECECSRO <casaraog@gmail.com>
2017-11-17 23:39:32 +0100

    1st - Supermarket

---
+++ Supermarket/Supermarket.cpp
@@ -0,0 +1,18 @@
+/*
+ * Supermarket.cpp
+ *
+ *  Created on: 17 nov. 2017
+ *      Author: ECECSRO
+ */
+
+#include "Supermarket.h"
+
+Supermarket::Supermarket() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Supermarket::~Supermarket() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Supermarket/Supermarket.h
@@ -0,0 +1,17 @@
+/*
+ * Supermarket.h
+ *
+ *  Created on: 17 nov. 2017
+ *      Author: ECECSRO
+ */
+
+#ifndef MASTER_PATTERNS_SUPERMARKET_SUPERMARKET_H_
+#define MASTER_PATTERNS_SUPERMARKET_SUPERMARKET_H_
+
+class Supermarket {
+public:
+	Supermarket();
+	virtual ~Supermarket();
+};
+
+#endif /* MASTER_PATTERNS_SUPERMARKET_SUPERMARKET_H_ */
